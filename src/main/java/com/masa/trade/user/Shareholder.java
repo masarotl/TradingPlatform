75900831d3ab6981fdc0771d4e1323cfa20f4c10
mark.russello <mark.russello@ACLLON607.fdmgroup.local>
2012-04-30 16:17:32 +0100

    Mark Changed Main

--- src/main/java/com/masa/trade/user/Shareholder.java
+++ src/main/java/com/masa/trade/user/Shareholder.java
@@ -1,22 +1,26 @@
 package com.masa.trade.user;
 
 public class Shareholder {
-	
+
 	private String name;
-	
-	public Shareholder(String id){
+
+	public Shareholder(String id) {
 		name = id;
 	}
-	
+
 	public void makeRequest() {
-		
+
 	}
-	
-	public void thisIsReallyCool(){
-		
+
+	public void thisIsReallyCool() {
+
 	}
-	
+
 	public static void main(String... args) {
-		System.out.println("Shareholder class");
+		System.out.println("Shareholder class " + new Shareholder("MARK").getName());
+	}
+	
+	public String getName(){
+		return name;
 	}
 }
