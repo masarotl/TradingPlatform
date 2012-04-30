1957b77769ece680866e24d49defea95d2bd0aa6
sherman.tam <sherman.tam@ACLLON1525.fdmgroup.local>
2012-04-30 15:38:42 +0100

    Added new method makeRequest()

--- src/main/java/com/masa/trade/user/Shareholder.java
+++ src/main/java/com/masa/trade/user/Shareholder.java
@@ -1,6 +1,10 @@
 package com.masa.trade.user;
 
 public class Shareholder {
+	public void makeRequest() {
+		
+	}
+	
 	public static void main(String... args) {
 		System.out.println("Shareholder class");
 	}
